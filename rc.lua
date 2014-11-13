d09c747425d373fb2ad7c9d0040185691ca5353d
Agus Lopez <aremuinan@gmail.com>
2014-11-13 16:23:18 +0100

    Revert "AwesomeWM > rc.lua: Honor size hints for clients"
    This reverts commit 85284781b8a26319826edb031a2bb752f71d3690.

--- rc.lua
+++ rc.lua
@@ -113,7 +113,7 @@ shifty.config.apps = {
     { match = { "^gimp%-dock$" }, geometry = {1105,15,175,770}, slave = true, border_width = 0 },
     -- client manipulation
     { match = { "" },
-        honorsizehints = true,
+        honorsizehints = false,
         buttons = awful.util.table.join (
         awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
         awful.button({ altkey }, 1, awful.mouse.client.move),
