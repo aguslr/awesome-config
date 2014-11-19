94ca88ceb36b898c57f43e0fadfd2babb4b1e2b1
Agus Lopez <aremuinan@gmail.com>
2014-11-19 16:44:56 +0100

    AwesomeWM > rc.lua: Add keybinding to see current window information

--- rc.lua
+++ rc.lua
@@ -733,6 +733,32 @@ globalkeys = awful.util.table.join(
                   mypromptbox[mouse.screen].widget,
                   awful.util.eval, nil,
                   awful.util.getdir("cache") .. "/history_eval")
+              end),
+
+    -- Print info on current client
+    awful.key({ modkey }, "i",
+              function ()
+                  local c = client.focus
+                  if not c then
+                      return
+                  end
+
+                  local geom = c:geometry()
+
+                  local t = ""
+                  if c.class then t = t .. "Class: " .. c.class .. "\n" end
+                  if c.instance then t = t .. "Instance: " .. c.instance .. "\n" end
+                  if c.role then t = t .. "Role: " .. c.role .. "\n" end
+                  if c.name then t = t .. "Name: " .. c.name .. "\n" end
+                  if c.type then t = t .. "Type: " .. c.type .. "\n" end
+                  if geom.width and geom.height and geom.x and geom.y then
+                      t = t .. "Dimensions: " .. "x:" .. geom.x .. " y:" .. geom.y .. " w:" .. geom.width .. " h:" .. geom.height
+                  end
+
+                  naughty.notify({
+                      text = t,
+                      timeout = 10,
+                  })
               end)
 
     -- Modules
