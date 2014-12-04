a1b81bd019341a148699906763a7f2955aae72a0
Agus Lopez <aremuinan@gmail.com>
2014-12-04 13:09:17 +0100

    AwesomeWM > theme.lua: Match colors

--- themes/my.theme/theme.lua
+++ themes/my.theme/theme.lua
@@ -4,14 +4,14 @@ theme = {}
 
 theme.font          = "terminus 9"
 
-theme.bg_normal     = "#111313"
+theme.bg_normal     = "#000000"
 theme.bg_focus      = "#111313"
 theme.bg_urgent     = "#111313"
 theme.bg_minimize   = "#111313"
 
-theme.fg_normal     = "#424446"
+theme.fg_normal     = "#b8baba"
 theme.fg_focus      = "#6c8b9e"
-theme.fg_urgent     = "#b8baba"
+theme.fg_urgent     = "#b290b6"
 theme.fg_minimize   = "#424446"
 
 theme.border_width  = "1"
@@ -19,8 +19,18 @@ theme.border_normal = "#424446"
 theme.border_focus  = "#b8baba"
 theme.border_marked = "#986b9e"
 
+-- MENU
+theme.menu_submenu_icon = awful.util.getdir("config") .. "/themes/my.theme/submenu.png"
+theme.menu_height       = "14"
+theme.menu_width        = "100"
+theme.menu_bg_normal    = "#000000"
+theme.menu_bg_focus     = "#111313"
+theme.menu_fg_normal    = "#b8baba"
+theme.menu_fg_focus     = "#6c8b9e"
+theme.menu_border_color = "#000000"
+theme.menu_border_width = "1"
+
 -- TAGLIST
---theme.taglist_bg_focus      = "#333333"
 theme.taglist_squares_sel   = awful.util.getdir("config") .. "/themes/my.theme/taglist/squarefw.png"
 theme.taglist_squares_unsel = awful.util.getdir("config") .. "/themes/my.theme/taglist/squarew.png"
 
@@ -43,17 +53,6 @@ theme.layout_gimp          = awful.util.getdir("config") .. "/vain/themes/defaul
 theme.layout_cascade       = awful.util.getdir("config") .. "/vain/themes/default/layouts/cascadew.png"
 theme.layout_cascadebrowse = awful.util.getdir("config") .. "/vain/themes/default/layouts/cascadebrowsew.png"
 theme.layout_centerwork    = awful.util.getdir("config") .. "/vain/themes/default/layouts/centerworkw.png"
-
--- MENU
-theme.menu_submenu_icon = awful.util.getdir("config") .. "/themes/my.theme/submenu.png"
-theme.menu_height       = "14"
-theme.menu_width        = "100"
-theme.menu_bg_normal    = "#000000"
-theme.menu_bg_focus     = "#111313"
-theme.menu_fg_normal    = "#b8baba"
-theme.menu_fg_focus     = "#6c8b9e"
-theme.menu_border_color = "#000000"
-theme.menu_border_width = "1"
 
 -- WALLPAPER
 --theme.wallpaper_cmd = { "awsetbg -c " .. os.getenv("HOME") .. "/Imágenes/wallpaper" }
