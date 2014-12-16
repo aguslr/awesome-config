10c3f853b99d8dc4620ffb8b996a22a27ea32e9e
Agus Lopez <aremuinan@gmail.com>
2014-12-16 10:55:34 +0100

    AwesomeWM > rc.lua: Launch RSI prevention tool on login

--- rc.lua
+++ rc.lua
@@ -1003,6 +1003,8 @@ run_once("mpd")
 run_once("owncloud")
 -- Start clipboard manager
 run_once("parcellite")
+-- Start RSI prevention tool
+run_once("workrave")
 -- Start lightsOn script
 run_once("lightsOn.sh", os.getenv("HOME") .. "/bin/lightsOn.sh 300")
 
