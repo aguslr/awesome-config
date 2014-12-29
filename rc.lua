677bb955e5f0291e30871d6bf29c241edfb10bfe
Agus Lopez <aremuinan@gmail.com>
2014-12-29 11:31:56 +0100

    AwesomeWM > rc.lua: make update-manager float

--- rc.lua
+++ rc.lua
@@ -142,7 +142,7 @@ shifty.config.apps = {
     { match = { "abiword", "gnumeric", "LibreOffice.*" }, tag = "7:office", },
     { match = { "Administrador de Máquina Virtual", "virt-manager", "VirtualBox" }, tag = "8:virt", },
     -- Floating clients
-    { match = { "alsamixer", "bash", "Empathy", "htop", "ImageMagick", "keepassx", "MPlayer", "mc", "mutt", "Nautilus", "ncmpcpp", "Pcmanfm", "ristretto", "sensors", "Sonata", "Thunar", "Transmission", "uGet", "watch", "wyrd" }, float = true },
+    { match = { "alsamixer", "bash", "Empathy", "htop", "ImageMagick", "keepassx", "MPlayer", "mc", "mutt", "Nautilus", "ncmpcpp", "Pcmanfm", "ristretto", "sensors", "Sonata", "Thunar", "Transmission", "uGet", "Update Manager", "watch", "wyrd" }, float = true },
     -- client manipulation
     { match = { "" },
         honorsizehints = false
